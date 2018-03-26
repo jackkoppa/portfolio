3bcc6d3495e1b28f3ea2a2f74ad895d781efe0b8
Jack Koppa <hello@jackkoppa.com>
2018-03-26 07:17:40 -0500

    Add simple reusable Header With Link components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 // This is the Link API
 import Link from 'next/link'
+import Header from '../components/Header'
 
 const Index = () => (
     <div>
-        <Link href="/about">
-            <button>About Page</button>
-        </Link>
+        <Header />
         <p>Hello Next.js</p>
     </div>
 )
